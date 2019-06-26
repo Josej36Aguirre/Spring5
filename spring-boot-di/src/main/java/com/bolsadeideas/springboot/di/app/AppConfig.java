da4ac1698e0cb59c26dad3529a88d4dcd6ebb5ef
jose.aguirre <josej36.aguirre@gmail.com>
2019-06-26 12:53:28 -0500

    Nuevo metodo com mas items
    se hace inyeccion de dependencias

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
@@ -35,5 +35,19 @@ public class AppConfig {
 		ItemFactura linea2 = new ItemFactura(producto2, 4);
 		return Arrays.asList(linea1, linea2);
 	}
+	@Bean("itemsFacturaOficina")
+	@Primary
+	public List<ItemFactura> registrarItemsOficina(){
+		Producto producto1 = new Producto("Monitor LG LCD 24",150000);
+		Producto producto2 = new Producto("Notebook Asus",1600000);
+		Producto producto3 = new Producto("Impresora HP Multifuncional",1600000);
+		Producto producto4 = new Producto("EScritorio Oficina",1600000);
+		
+		ItemFactura linea1 = new ItemFactura(producto1, 2);
+		ItemFactura linea2 = new ItemFactura(producto2, 1);
+		ItemFactura linea3 = new ItemFactura(producto3, 1);
+		ItemFactura linea4 = new ItemFactura(producto4, 1);
+		return Arrays.asList(linea1, linea2, linea3, linea4);
+	}
 
 }
--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
@@ -14,6 +14,7 @@ public class Factura {
 	
 	@Autowired
 	private Cliente cliente;
+	
 	@Autowired
 	private List<ItemFactura> items;
 
