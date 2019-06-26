e6a8fa0222e8a39dd4eccfb74318577c793222cc
jose.aguirre <josej36.aguirre@gmail.com>
2019-06-26 14:13:02 -0500

    requesScope
    Inyectar dependecias con RequestScope

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
@@ -1,19 +1,38 @@
 package com.bolsadeideas.springboot.di.app.models.domain;
 
+import java.io.Serializable;
 import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Component
-public class Factura {
+@RequestScope
+public class Factura implements Serializable {
+
+	
+	private static final long serialVersionUID = 946004357128146951L;
 
 	@Value("${factura.descripcion}")
 	private String descripcion;
 	
 	@Autowired
 	private Cliente cliente;
+	
+	@PostConstruct
+	public void inicializar() {
+		cliente.setNombre(cliente.getNombre().concat(" ").concat("Aguirre"));
+		descripcion = descripcion.concat("del cliente: ").concat(cliente.getNombre());
+	}
+	@PreDestroy
+	public void destruir() {
+		System.out.println("Facturadestruida: ".concat(descripcion));
+	}
 	
 	@Autowired
 	private List<ItemFactura> items;
